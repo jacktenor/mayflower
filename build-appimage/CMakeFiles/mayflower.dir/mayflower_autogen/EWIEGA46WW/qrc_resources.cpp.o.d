0de0903be01200f112618a0e2877cc342a58bcde
jacktenor <jacktenor1@gmail.com>
2025-10-23 13:20:35 -0400

    sync

--- build-appimage/CMakeFiles/mayflower.dir/mayflower_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/mayflower.dir/mayflower_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /src/build-appimage/mayflower_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/Qt_6_9_0_6_9_0_static_fc-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_mayflower_FILE /home/john/dev/qt/mayflower/build/Qt_6_9_0_6_9_0_static_fc-Debug/mayflower)
-set(__QT_DEPLOY_TARGET_mayflower_TYPE EXECUTABLE)
